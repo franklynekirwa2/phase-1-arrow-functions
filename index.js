68c52e27fc657ff8c95d0f320be93f96fedbe79e
franklynekirwa2 <sangkirwa2@gmail.com>
2024-06-27 22:00:57 +0300

    arrowfunctions

---
+++ index.js
@@ -0,0 +1,21 @@
+const divide=(num1, num2)=>{
+    return num1/num2 ;
+};
+let num1=2000;
+let num2=100;
+console.log(divide);
+
+const square=(x)=>{
+    return x*x;
+};
+let x=2;
+console.log(square);
+
+const add=(a, b)=>{
+return a+b;
+};
+let a =3;
+let b =4;
+
+console.log(add(a,b));
+
